59439524e264e50eaccd405d511a9a72d4cacfa8
Nickddb <nicollerocha.dev2024@gmail.com>
2026-04-30 21:08:46 -0300

    atualizando menu

--- GABRIEL/aula9-DWEB/index.js
+++ GABRIEL/aula9-DWEB/index.js
@@ -1,20 +1,55 @@
+//System.out.println("Hello World!")
+console.log("Hello World!")
+
+/*
+Javascript != Java 
+Javascript não tem relação com o Java.
+Javascript usada primariamente para Navegadores WEB
+(Chrome/Internet Explorer/ Edge/ Mozila ...)
+
+TIPAGEM
+Fortemente tipada => Java, C# => Passe os tipos de todas as variáveis (POO)
+Fracamente tipada => Js, Python => Não exige tipo na declaração e manutenção das variáveis
+
+//Java
+int teste = 0;
+teste = 10; ok
+teste = "hello world!"  //string
+Erro de tipagem: Você declarou inteiro, e passou um TIPO diferente
+
+//JS
+var teste = 0;
+teste = "Hello World!"; ok
+teste = true; ok
+teste = null; ok
+
+var/let => Para declarar valores variáveis
+const => Para declarar valores constantes
+
+Qual é a diferença entre atribuição e comparação?
+atribuição é ATRIBUIR VALOR
+comparação é COMPARAR VALOR
+*/
 const botao = document.getElementById("entrar")
-const form = document.getElementById("loginForm")
+const formulario = document.getElementById("loginForm")
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
+formulario.addEventListener("submit" , (e)=>{
+    //Lógica do que deve acontecer, vem aqui.
+    e.preventDefault() //Impede da página recarregar
 
+    //coletar os dados da tela
     const email = document.getElementById("email").value
     const senha = document.getElementById("senha").value
-
-    if(email === "" || senha === ""){
-        alert("Erro na validação!")
+    
+    //Validar email e senha
+    if(email === "" || senha ===""){
+        alert("Credenciais inválidas")
     }
     else{
-        alert("Formulário enviado!")
+        alert("Formulário Enviado!")
         console.log({
             email: email,
             senha: senha
-    })
+        })
     }
 })
